ce5828346dc1dfbbab6cfbf45f1b8fd4efb3f29b
woooo96 <chsami3@naver.com>
2023-01-27 20:36:52 +0900

    chore : 댓글 조회 순서 변경

--- src/main/java/com/example/alba_pocket/repository/CommentRepository.java
+++ src/main/java/com/example/alba_pocket/repository/CommentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByPostIdOrderByCreatedAtDesc(Long postId);
+    List<Comment> findAllByPostIdOrderByCreatedAtAsc(Long postId);
     int countByPostId(Long postId);
 
 }
