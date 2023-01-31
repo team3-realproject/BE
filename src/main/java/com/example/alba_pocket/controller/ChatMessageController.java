33c7e3e8c379654353b3fdcdc7f1b32a7489109e
wogk9854 <wogk9843@naver.com>
2023-01-31 23:48:13 +0900

    chore : MessageController @RequestMapping 제거

--- src/main/java/com/example/alba_pocket/controller/ChatMessageController.java
+++ src/main/java/com/example/alba_pocket/controller/ChatMessageController.java
@@ -6,28 +6,27 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
-@RequestMapping("/api/chat")
+@RestController
+//@RequestMapping("/api/chat")
 @Slf4j
 public class ChatMessageController {
 
     private final ChatMessageService chatMessageService;
 
     //  /pub/api/chat/message 요청처리 /sub
-    @MessageMapping("/message")
+    @MessageMapping("/api/chat/message")
     public void message(ChatMessageRequestDto message) {
         log.info("-------  MessageMapping ----------");
         chatMessageService.message(message);
     }
 
     //이전채팅내역조회
-    @GetMapping("/message/{roomId}")
+    @GetMapping("/api/chat/message/{roomId}")
     public ResponseEntity<?> getMessage(@PathVariable String roomId){
         return chatMessageService.getMessage(roomId);
     }
