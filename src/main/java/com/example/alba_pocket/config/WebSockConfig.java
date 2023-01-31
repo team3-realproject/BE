69c674478073a3bc7893a83336c456e3e95e704d
wogk9854 <wogk9843@naver.com>
2023-01-31 21:05:19 +0900

    chore : cors설정추가

--- src/main/java/com/example/alba_pocket/config/WebSockConfig.java
+++ src/main/java/com/example/alba_pocket/config/WebSockConfig.java
@@ -18,7 +18,7 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat")
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 }
