802c6547aabcb1c1074c35cfe5efc157b69ffb3f
wogk9854 <wogk9843@naver.com>
2023-02-04 00:00:22 +0900

    feat : 이메일인증추가

---
+++ src/main/java/com/example/alba_pocket/dto/EmailCheckDto.java
@@ -0,0 +1,10 @@
+package com.example.alba_pocket.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailCheckDto {
+    public String email;
+}
