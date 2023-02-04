e95b050ccc74f9a63634d98c0c470e7777b822f5
wogk9854 <wogk9843@naver.com>
2023-02-04 21:25:34 +0900

    fix : 채팅방리스트 최신방부터 나오도록 정렬

--- src/main/java/com/example/alba_pocket/dto/RoomIdResponseDto.java
+++ src/main/java/com/example/alba_pocket/dto/RoomIdResponseDto.java
@@ -1,11 +1,10 @@
 package com.example.alba_pocket.dto;
 
-import com.example.alba_pocket.entity.ChatMessage;
-import com.example.alba_pocket.entity.ChatRoom;
+import com.example.alba_pocket.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.util.Map;
 
 @Getter
 @NoArgsConstructor
@@ -18,16 +17,23 @@ public class RoomIdResponseDto {
 
     private String lastMessage;
 
-    private LocalDateTime createdAt;
+    private String createdAt;
 
-    public RoomIdResponseDto(ChatRoom roomList, ChatMessage chatMessage) {
-        this.roomId = roomList.getRoomId();
-        this.nickname = roomList.getToUser().getNickname();
-        this.profileImage = roomList.getToUser().getProfileImage();
-        this.lastMessage = chatMessage.getMessage();
-        this.createdAt = chatMessage.getCreatedAt();
+//    public RoomIdResponseDto(ChatRoom roomList, ChatMessage chatMessage) {
+//        this.roomId = roomList.getRoomId();
+//        this.nickname = roomList.getToUser().getNickname();
+//        this.profileImage = roomList.getToUser().getProfileImage();
+//        this.lastMessage = chatMessage.getMessage();
+//        this.createdAt = String.valueOf(chatMessage.getCreatedAt());
+//    }
+
+
+    public RoomIdResponseDto(Map<Object, Object> roomList, User toUser) {
+        this.roomId = (String) roomList.get("room_id");
+        this.nickname = toUser.getNickname();
+        this.profileImage = toUser.getProfileImage();
+        this.lastMessage = String.valueOf(roomList.get("message"));
+        this.createdAt = String.valueOf(roomList.get("created_at"));
     }
-
-
 
 }
